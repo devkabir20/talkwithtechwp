70e778630fcb7f377fc525231189305fa49b5acb
Md Kabir Hasan <hk19kabir@gmail.com>
2022-08-30 15:59:01 +0600

    Update

--- footer.php
+++ footer.php
@@ -1,19 +1,6 @@
-<?php
-/**
- * The template for displaying the footer
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package TalkWithTech
- */
 
-?>
-
-</div><!-- #page -->
-
-<?php wp_footer(); ?>
+<footer></footer>
 
 </body>
+
 </html>
--- template-parts/home-page.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Template Name: Home
-
-get_header();
-?>
-<?php get_footer();?>
